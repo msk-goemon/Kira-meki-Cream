2c6bef45c353a4b63b9705ac7e347e24dd5daabd
msk-goemon <goemon621@gmail.com>
2024-04-20 19:41:36 +0900

    slick、余白の微調節

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,29 @@
+$(function(){
 $('.voices-items__inner').slick({
   infinite: true, // 無限ループスライド
-  dots: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 300,
   slidesToShow: 3, // 一度に表示するスライド数
-  slidesToScroll: 1, // 一度にスクロールするスライド数
+  slidesToScroll: 3, // 一度にスクロールするスライド数
   // prevArrow: '<img src="../image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
   // nextArrow: '<img src="../image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
   responsive: [ // レスポンシブ設定
-      {
+        {
           breakpoint: 1024, // この幅以下で切り替える
-          settings: {
+            settings: {
               slidesToShow: 2, // 中幅のデバイスでは2枚表示
-          }
-      },
-      {
-          breakpoint: 600,
-          settings: {
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
               slidesToShow: 1, // 小幅のデバイスでは1枚表示
-          }
-      }
-  ]
+            }
+        }
+    ]
 });
-
+});
 
 
 $(function(){
