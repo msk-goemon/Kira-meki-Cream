62d29f73459e955b5b3dab6bf5c36e9776bc3b02
msk-goemon <goemon621@gmail.com>
2024-04-20 19:42:00 +0900

    調整

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,24 +1,24 @@
 $(function(){
 $('.voices-items__inner').slick({
   infinite: true, // 無限ループスライド
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 300,
+  dots: true,
   slidesToShow: 3, // 一度に表示するスライド数
   slidesToScroll: 3, // 一度にスクロールするスライド数
-  // prevArrow: '<img src="../image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
-  // nextArrow: '<img src="../image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
+  prevArrow: '<img src="assets/image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="assets/image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
   responsive: [ // レスポンシブ設定
         {
-          breakpoint: 1024, // この幅以下で切り替える
+          breakpoint: 1370, // この幅以下で切り替える
             settings: {
               slidesToShow: 2, // 中幅のデバイスでは2枚表示
+              slidesToScroll: 2,
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 500,
             settings: {
               slidesToShow: 1, // 小幅のデバイスでは1枚表示
+              slidesToScroll: 1,
             }
         }
     ]
@@ -32,4 +32,15 @@ $(function(){
         $(this).toggleClass("open");
         return false;
     });
+});
+
+
+// トップへ戻るボタンのコード
+$(document).ready(function(){
+  $('.js__top-button').click(function(){
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800); // 800ミリ秒（0.8秒）かけてトップへスクロール
+    return false; // デフォルトのイベントをキャンセル
+  });
 });
