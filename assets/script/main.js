12bd34b590eb25466bbbb8e2a2604338f040c349
msk-goemon <goemon621@gmail.com>
2024-04-24 20:15:10 +0900

    インデント

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,28 +1,28 @@
 $(function(){
-$('.voices-items__inner').slick({
-  infinite: true, // 無限ループスライド
-  dots: true,
-  slidesToShow: 3, // 一度に表示するスライド数
-  slidesToScroll: 3, // 一度にスクロールするスライド数
-  prevArrow: '<img src="assets/image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
-  nextArrow: '<img src="assets/image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
-  responsive: [ // レスポンシブ設定
-        {
-          breakpoint: 1370, // この幅以下で切り替える
-            settings: {
-              slidesToShow: 2, // 中幅のデバイスでは2枚表示
-              slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoint: 500,
-            settings: {
-              slidesToShow: 1, // 小幅のデバイスでは1枚表示
-              slidesToScroll: 1,
-            }
-        }
-    ]
-});
+    $('.voices-items__inner').slick({
+        infinite: true, // 無限ループスライド
+        dots: true,
+        slidesToShow: 3, // 一度に表示するスライド数
+        slidesToScroll: 3, // 一度にスクロールするスライド数
+        prevArrow: '<img src="assets/image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
+        nextArrow: '<img src="assets/image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
+        responsive: [ // レスポンシブ設定
+              {
+                breakpoint: 1370, // この幅以下で切り替える
+                  settings: {
+                    slidesToShow: 2, // 中幅のデバイスでは2枚表示
+                    slidesToScroll: 2,
+                  }
+              },
+              {
+                  breakpoint: 500,
+                  settings: {
+                    slidesToShow: 1, // 小幅のデバイスでは1枚表示
+                    slidesToScroll: 1,
+                  }
+              }
+          ]
+    });
 });
 
 
@@ -37,10 +37,10 @@ $(function(){
 
 // トップへ戻るボタンのコード
 $(document).ready(function(){
-  $('.js__top-button').click(function(){
-    $('html, body').animate({
-      scrollTop: 0
-    }, 800); // 800ミリ秒（0.8秒）かけてトップへスクロール
-    return false; // デフォルトのイベントをキャンセル
-  });
+    $('.js__top-button').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800); // 800ミリ秒（0.8秒）かけてトップへスクロール
+        return false; // デフォルトのイベントをキャンセル
+    });
 });
