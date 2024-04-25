329abdf7fac5177e343ea4557daff952a6e9f64e
msk-goemon <goemon621@gmail.com>
2024-04-25 21:14:03 +0900

    インデント、breakpoint修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,21 +7,21 @@ $(function(){
         prevArrow: '<img src="assets/image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
         nextArrow: '<img src="assets/image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
         responsive: [ // レスポンシブ設定
-              {
+            {
                 breakpoint: 1370, // この幅以下で切り替える
-                  settings: {
+                settings: {
                     slidesToShow: 2, // 中幅のデバイスでは2枚表示
                     slidesToScroll: 2,
-                  }
-              },
-              {
-                  breakpoint: 500,
-                  settings: {
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
                     slidesToShow: 1, // 小幅のデバイスでは1枚表示
                     slidesToScroll: 1,
-                  }
-              }
-          ]
+                }
+            }
+        ]
     });
 });
 
